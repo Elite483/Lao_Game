19a15da4ff620762ea1e409f88ae691bc217d5ba
Fabrizzioolariaga <fabrizzioolariaga88@gmail.com>
2026-09-09 20:04:18 -0300

    Test push desde facultad

---
+++ Assets/Scripts/ScriptPruebaGitHub.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScriptPruebaGitHub : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("TEST - BORRAR ESTE SCRIPT.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
